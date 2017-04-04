d5989d0fa42150c79a42b76380bb9cf680629799
kraken <root@sunsongs.ru>
2017-04-04 13:24:50 +0500

    resolving exclamation sign in dependency name

--- embedded-require.js
+++ embedded-require.js
@@ -1,146 +1,187 @@
-(function(){
+(function () {
 
-	var dependenciesCache = (function(){
-		var pending = [];
-		var resolved = new Map();
+    const dependenciesCache = (function () {
+        let pending = [];
+        const resolved = new Map();
+        const delegated = new Set();
 
-		function add(name, dependencies, factory) {
-			console.log(`Registered: ${name}`);
+        function add(name, dependencies, factory) {
+            console.log(`Registered: ${name}`);
 
-			var module = execute(factory, dependencies);
-			if (module === false) {
-				addToPending(name, dependencies, factory);
-			} else {
-				console.log(`Executed: ${name}`);
-				addToResolved(name, module);
-				resolvePendings();
-			}
+            dependencies = resolveExclamation(dependencies);
 
-			console.log(`Pending dependencies count: ${pending.length}`);
-		}
+            const module = execute(factory, dependencies);
+            if (module === false) {
+                addToPending(name, dependencies, factory);
+            } else {
+                console.log(`Executed: ${name}`);
+                addToResolved(name, module);
+                resolvePendings();
+            }
 
-		function getDependencies(dependencies) {
-			return dependencies.map(function(d) {
-				var module = resolved.get(d);
-				return module === undefined ? null : module;
-			}).filter(d => d !== null);
-		}
+            console.log(`Pending dependencies count: ${pending.length}`);
+        }
 
-		function execute(factory, dependencies) {
-			var resolvedDependencies = getDependencies(dependencies)
-			if (dependencies.length !== resolvedDependencies.length) {
-				return false;
-			} else {
-				return factory.apply(this, resolvedDependencies);
-			}
-		}
+        function resolveExclamation(dependencies) {
+            return dependencies.map(d => {
+                if (d.indexOf('!') > 0) {
+                    const parts = d.split('!');
+                    const module = parts[0];
+                    const loadItem = parts[1];
+                    const depName = `${module}_load_${loadItem}`;
+                    const delegatedDep = `${module}_delegated_${loadItem}`;
+                    const delegatedWaitDep = `${module}_delegated_wait_${loadItem}`;
 
-		function addToPending(name, dependencies, factory) {
-			pending.push({name: name, dependencies: dependencies, factory: factory});
-		}
+                    delegated.add(delegatedDep);
+                    add(depName, [module], (m) => m.load(loadItem, {toUrl: urlFor}, resoveDelegated(delegatedDep), {}));
+                    add(delegatedWaitDep, [delegatedDep], dd => dd);
 
-		function addToResolved(name, module) {
-			resolved.set(name, module);
-		}
+                    return delegatedWaitDep;
+                } else {
+                    return d;
+                }
+            });
+        }
 
-		// TODO can be optimized. For now every execution will iterate over pending list and try to resolve deps for each pending module
-		function resolvePendings() {
-			pending = pending.filter(e => {
-				var module = execute(e.factory, e.dependencies);
-				if (module === false) {
-					return true;
-				} else {
-					console.log(`Executed: ${e.name}`);
-					addToResolved(e.name, module);
-					return false;
-				}
-			});
-		}
+        function resoveDelegated(name) {
+            return function (val) {
+                delegated.delete(name);
+                resolved.set(name, val);
+                resolvePendings();
+            }
+        }
 
-		function renameDependency(name, newName) {
-			// rename if dependency has been resolved
-			var module = resolved.get(name);
-			if (typeof module !== 'undefined') {
-				resolved.delete(name);
-				resolved.set(newName, module);
-				console.log(`resolved dependency ${name} was renamed to ${newName}`)
-			}
-			
-			// rename if dependency is pending
-			pending.forEach(d => {
-				if (d.name === name) {
-					console.log(`Unresolved dependency ${name} was renamed to ${newName}`)
-					d.name = newName
-				}
-			})
-		}
+        function urlFor(name) {
+            return $('script[src]')
+                .map(function (s) {
+                    return $(this).attr('src')
+                })
+                .toArray()
+                .filter(s => typeof s === 'string' && s.endsWith(name))[0];
+        }
 
-		return {
-			add: add,
-			renameDependency: renameDependency
-		}
-	})();
+        function getDependencies(dependencies) {
+            return dependencies.map(d => {
+                const module = resolved.get(d);
+                return module === undefined ? null : module;
+            }).filter(d => d !== null);
+        }
 
-	var nameGenerator = (function(){
-		var id = 0;
+        function execute(factory, dependencies) {
+            const resolvedDependencies = getDependencies(dependencies);
+            if (dependencies.length !== resolvedDependencies.length) {
+                return false;
+            } else {
+                return factory.apply(this, resolvedDependencies);
+            }
+        }
 
-		function generateName(n) {
-			return `undefined_${id}`;
-		}
+        function addToPending(name, dependencies, factory) {
+            pending.push({name: name, dependencies: dependencies, factory: factory});
+        }
 
-		function next() {
-			id += 1;
-			return generateName(id);
-		}
+        function addToResolved(name, module) {
+            resolved.set(name, module);
+        }
+
+        // TODO can be optimized. For now every execution will iterate over pending list and try to resolve deps for each pending module
+        function resolvePendings() {
+            pending = pending.filter(e => {
+                const module = execute(e.factory, e.dependencies);
+                if (module === false) {
+                    return true;
+                } else {
+                    console.log(`Executed: ${e.name}`);
+                    addToResolved(e.name, module);
+                    return false;
+                }
+            });
+        }
+
+        function renameDependency(name, newName) {
+            // rename if dependency has been resolved
+            const module = resolved.get(name);
+            if (typeof module !== 'undefined') {
+                resolved.delete(name);
+                resolved.set(newName, module);
+                console.log(`resolved dependency ${name} was renamed to ${newName}`)
+            }
+
+            // rename if dependency is pending
+            pending.forEach(d => {
+                if (d.name === name) {
+                    console.log(`Unresolved dependency ${name} was renamed to ${newName}`);
+                    d.name = newName
+                }
+            })
+        }
+
+        return {
+            add: add,
+            renameDependency: renameDependency
+        }
+    })();
+
+    const nameGenerator = (function () {
+        let id = 0;
+
+        function generateName() {
+            return `undefined_${id}`;
+        }
+
+        function next() {
+            id += 1;
+            return generateName();
+        }
 
 
-		function last() {
-			return generateName(id);
-		}
+        function last() {
+            return generateName();
+        }
 
-		return {
-			next: next,
-			last: last
-		}
-	})();
+        return {
+            next: next,
+            last: last
+        }
+    })();
 
 
-	if (typeof window.define === 'undefined') {
-		
-		window.define = function (name, dependencies, factory) {
-			// if no name and dependencies specified
-			if (typeof name !== "string") {
-				factory = name;
-				name = nameGenerator.next();
-				dependencies = [];
-			}
+    if (typeof window.define === 'undefined') {
 
-			// if no dependencies specified
-			if (typeof dependencies === "function") {
-				factory = dependencies;
-				dependencies = [];
-			}
+        window.define = function (name, dependencies, factory) {
+            // if no name and dependencies specified
+            if (typeof name !== "string") {
+                factory = name;
+                name = nameGenerator.next();
+                dependencies = [];
+            }
 
-			// add to cache
-			if (typeof factory === "function") {
-				dependenciesCache.add(name, dependencies, factory);
-			} else {
-				dependenciesCache.add(name, dependencies, () => factory);
-			}
-		}
+            // if no dependencies specified
+            if (typeof dependencies === "function") {
+                factory = dependencies;
+                dependencies = [];
+            }
 
-		window.define.amd = true;
+            if (typeof factory === "function") {
+                dependenciesCache.add(name, dependencies, factory);
+            } else {
+                dependenciesCache.add(name, dependencies, () => factory);
+            }
+        };
 
-		window.defineLast = function(name) {
-			dependenciesCache.renameDependency(nameGenerator.last(), name);
-		}
+        window.define.amd = true;
 
-		window.require = function (dependencies, factory) {
-			var name = nameGenerator.next() + "_required";
-			dependenciesCache.add(name, dependencies, factory)
-		}	
-	} else {
-		console.info('Method define was already defined')
-	}
+        window.defineLast = function (name) {
+            dependenciesCache.renameDependency(nameGenerator.last(), name);
+        };
 
-})(window.define)
+        window.require = function (dependencies, factory) {
+            const name = nameGenerator.next() + "_required";
+            dependenciesCache.add(name, dependencies, factory)
+        }
+
+    } else {
+        console.info('Method define was already defined')
+    }
+
+})(window.define);
